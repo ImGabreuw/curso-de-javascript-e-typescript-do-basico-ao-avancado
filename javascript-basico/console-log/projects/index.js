8d5c88bfd6f6a055156c11fd9e98fe1a8efbf339
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-01 13:21:58 -0300

    console.log

---
+++ javascript-basico/console-log/projects/index.js
@@ -0,0 +1,9 @@
+console.log("Luiz Otávio");
+console.log('Luiz Otávio');
+console.log('Luiz "Otávio"');
+console.log("Luiz 'Otávio'");
+console.log(`'Luiz' "Otávio"`);
+console.log(123456);
+console.log(12, 15.85);
+console.log(12, 15.85, "Luiz Otávio de novo");
+console.log(12, 15.85, 'Luiz Otávio de novo');
