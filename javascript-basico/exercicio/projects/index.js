2321fd87e48da4c2ef26d6fde211e75d3263390b
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-01 13:27:58 -0300

    JavaScript Basico: 'Exercicios'

---
+++ javascript-basico/exercicio/projects/index.js
@@ -0,0 +1,3 @@
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às 10 da manhã.');
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às', 10, 'da manhã.');
+console.log(`Meu nome é "NOME". Estou aprendendo JavaScript às 10 da manhã.`);
