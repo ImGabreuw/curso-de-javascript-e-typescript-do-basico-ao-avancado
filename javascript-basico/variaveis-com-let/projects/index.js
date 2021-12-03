19c23594fb95e9733b2d511d844b4e386171e8b3
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-03 17:07:05 -0300

    JavaScript Basico: 'Variáveis com let'

---
+++ javascript-basico/variaveis-com-let/projects/index.js
@@ -0,0 +1,8 @@
+// let nome = "João";
+let nome = "Henrique";
+
+console.log(nome, "nasceu em 1984.");
+console.log("Em 2000", nome, "conheceu Maria.");
+console.log(nome, "casou-se com Maria em 2012.");
+console.log("Maria teve 1 filho com", nome, "em 2015.");
+console.log("O filho de", nome, "se chama Eduardo.");
