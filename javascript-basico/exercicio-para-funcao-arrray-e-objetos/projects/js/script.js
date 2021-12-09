8a76eb124b9543371580940fb5e6ffa8a51f41a8
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-09 19:21:01 -0300

    JavaScript Basico: 'Exercício para função, array e objetos'

---
+++ javascript-basico/exercicio-para-funcao-arrray-e-objetos/projects/js/script.js
@@ -0,0 +1,28 @@
+function meuEscopo() {
+  const form = document.querySelector(".form");
+  const resultado = document.querySelector(".resultado");
+
+  const pessoas = [];
+
+  form.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    const nome = form.querySelector(".nome");
+    const sobrenome = form.querySelector(".sobrenome");
+    const peso = form.querySelector(".peso");
+    const altura = form.querySelector(".altura");
+
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value
+    });
+
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${altura.value} ${altura.value}</p>`
+  });
+}
+
+meuEscopo();
