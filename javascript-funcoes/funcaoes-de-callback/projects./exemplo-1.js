38dcff33d38a73d58abeefd19eefd6d4cd2b9cd2
ImGabreuw <imgabreuw.dev@gmail.com>
2021-12-17 16:40:43 -0300

    JavaScript Funçoes (Avançado): 'Funções de callback'

---
+++ javascript-funcoes/funcaoes-de-callback/projects./exemplo-1.js
@@ -0,0 +1,37 @@
+function rand(min = 1000, max = 3000) {
+  const num = Math.random() * (max - min) + min;
+
+  return Math.floor(num);
+}
+
+function f1(callback) {
+  setTimeout(function() {
+    console.log("f1");
+
+    if (callback) callback();
+  }, rand());
+}
+
+function f2(callback) {
+  setTimeout(function() {
+    console.log("f2");
+
+    if (callback) callback();
+  }, rand());
+}
+
+function f3(callback) {
+  setTimeout(function() {
+    console.log("f3");
+
+    if (callback) callback();
+  }, rand());
+}
+
+f1(function() {
+  f2(function() {
+    f3(function() {
+      console.log("Olá mundo!");
+    });
+  });
+});
