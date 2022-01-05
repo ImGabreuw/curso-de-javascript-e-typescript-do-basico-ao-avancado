27806e46efe41292caf813ad283c0a2e9f88e002
ImGabreuw <imgabreuw.dev@gmail.com>
2022-01-05 17:29:55 -0300

    JavaScript + Node - Projeto Agenda (com Express + MongoDB): 'Correção ao apagar contatos'

--- javascript-e-node-projeto-agenda-com-express-e-mongo/agenda/routes.js
+++ javascript-e-node-projeto-agenda-com-express-e-mongo/agenda/routes.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 
-route.get("/", homeController.index);
+const { loginRequired } = require('./src/middlewares/middleware');
 
-route.get("/login/index", loginController.index);
-route.post("/login/register", loginController.register)
+// Rotas da home
+route.get('/', homeController.index);
+
+// Rotas de login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+// Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
 
 module.exports = route;
--- javascript-e-node-projeto-agenda-com-express-e-mongo/agenda/src/models/LoginModel.js
+++ javascript-e-node-projeto-agenda-com-express-e-mongo/agenda/src/models/LoginModel.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcryptjs = require("bcryptjs");
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
-const LoginModel = mongoose.model("Login", LoginSchema);
+const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
   constructor(body) {
@@ -16,55 +16,65 @@ class Login {
     this.user = null;
   }
 
-  async register() {
-    this.validar();
+  async login() {
+    this.valida();
+    if(this.errors.length > 0) return;
+    this.user = await LoginModel.findOne({ email: this.body.email });
 
-    if (this.errors.length > 0) return;
+    if(!this.user) {
+      this.errors.push('Usuário não existe.');
+      return;
+    }
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida');
+      this.user = null;
+      return;
+    }
+  }
+
+  async register() {
+    this.valida();
+    if(this.errors.length > 0) return;
 
     await this.userExists();
 
-    if (this.errors.length > 0) return;
+    if(this.errors.length > 0) return;
 
-    try {
-      const salt = bcryptjs.genSaltSync();
-      
-      this.body.password = bcryptjs.hashSync(this.body.password, salt);
-      this.user = await LoginModel.create(this.body);
-    } catch (err) {
-      console.log(err);
-    }
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+    this.user = await LoginModel.create(this.body);
   }
 
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email });
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if(this.user) this.errors.push('Usuário já existe.');
+  }
 
-    if (user) {
-      this.errors.push("Usuário já existe.")
+  valida() {
+    this.cleanUp();
+
+    // Validação
+    // O e-mail precisa ser válido
+    if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+
+    // A senha precisa ter entre 3 e 50
+    if(this.body.password.length < 3 || this.body.password.length > 50) {
+      this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
     }
   }
 
-  validar() {
-    this.convertAllFieldsValueToString();
-
-    if (!validator.isEmail(this.body.email)) {
-      this.errors.push("E-mail inválido");
-    }
-
-    if (this.body.password.length < 3 || this.body.password.length > 50) {
-      this.errors.push("A senha precisar ter entre 3 a 50 caracteres");
-    }
-  }
-
-  convertAllFieldsValueToString() {
-    for (const key in this.body) {
-      if (typeof this.body[key] !== "string") {
-        this.body[key] = "";
+  cleanUp() {
+    for(const key in this.body) {
+      if(typeof this.body[key] !== 'string') {
+        this.body[key] = '';
       }
     }
 
     this.body = {
       email: this.body.email,
-      password: this.body.password,
+      password: this.body.password
     };
   }
 }
