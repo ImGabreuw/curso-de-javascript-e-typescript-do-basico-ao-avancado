76e86d66998b0a97c1ddbb58b565dbb8cf465f2c
ImGabreuw <imgabreuw.dev@gmail.com>
2022-01-11 19:45:58 -0300

    API REST - JavaScript, Node, Express e SQL (MySQL ou MariaDB): 'Tratando os arquivos enviados'

--- api-rest-javascript-node-express-e-sql-mysql-ou-mariadb/projects/api-rest/src/config/multer.js
+++ api-rest-javascript-node-express-e-sql-mysql-ou-mariadb/projects/api-rest/src/config/multer.js
@@ -5,6 +5,13 @@ const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
   storage: multer.diskStorage({
+    fileFilter: (req, file, callback) => {
+      if (file.mimetype !== "image/png" && file.mimetype !== "image/jpeg") {
+        return callback(new multer.MulterError("Arquivo precisa ser PNG ou JPG"));
+      }
+
+      return callback(null, true);
+    },
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, "..", ",,", "upload"));
     },
--- api-rest-javascript-node-express-e-sql-mysql-ou-mariadb/projects/api-rest/src/controllers/FotoController.js
+++ api-rest-javascript-node-express-e-sql-mysql-ou-mariadb/projects/api-rest/src/controllers/FotoController.js
@@ -1,6 +1,19 @@
+import multer from "multer";
+import multerConfig from "../config/multer";
+
+const upload = multer(multerConfig).single("foto");
+
 class FotoController {
   async store(req, res) {
-    res.json("Index");
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
