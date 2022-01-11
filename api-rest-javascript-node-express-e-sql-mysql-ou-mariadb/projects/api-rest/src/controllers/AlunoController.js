6b8a4cdee59ab0a4057be822eb81672c39e0720e
ImGabreuw <imgabreuw.dev@gmail.com>
2022-01-11 16:31:32 -0300

    API REST - JavaScript, Node, Express e SQL (MySQL ou MariaDB): 'CRUD de alunos'

--- api-rest-javascript-node-express-e-sql-mysql-ou-mariadb/projects/api-rest/src/controllers/AlunoController.js
+++ api-rest-javascript-node-express-e-sql-mysql-ou-mariadb/projects/api-rest/src/controllers/AlunoController.js
@@ -6,6 +6,100 @@ class AlunoController {
 
     return res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.status(201).json(aluno);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["ID é obrigatório"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["ID é obrigatório"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      await Aluno.destroy();
+
+      return res.status(204);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["ID é obrigatório"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      return res.json(alunoAtualizado);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
