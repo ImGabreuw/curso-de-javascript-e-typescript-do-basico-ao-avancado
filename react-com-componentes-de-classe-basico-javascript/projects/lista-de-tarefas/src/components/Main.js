9db5a095780f7164e74d536d64c1430474a62787
ImGabreuw <imgabreuw.dev@gmail.com>
2022-01-17 19:11:45 -0300

    React com componentes de classe (Básico) - JavaScript: 'Estilizando o form'

--- react-com-componentes-de-classe-basico-javascript/projects/lista-de-tarefas/src/components/Main.js
+++ react-com-componentes-de-classe-basico-javascript/projects/lista-de-tarefas/src/components/Main.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { FaPlus } from 'react-icons/fa';
+
 import './Main.css'
 
 export default class Main extends Component {
@@ -19,9 +22,15 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     )
