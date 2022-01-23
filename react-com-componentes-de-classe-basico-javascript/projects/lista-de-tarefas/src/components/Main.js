f252c9ec085c0c162f2079bced86039fb97f3cf8
ImGabreuw <imgabreuw.dev@gmail.com>
2022-01-23 17:49:48 -0300

    React com componentes de classe (Básico) - JavaScript: 'Editando tarefas'

--- react-com-componentes-de-classe-basico-javascript/projects/lista-de-tarefas/src/components/Main.js
+++ react-com-componentes-de-classe-basico-javascript/projects/lista-de-tarefas/src/components/Main.js
@@ -11,12 +11,13 @@ import './Main.css'
 export default class Main extends Component {
   state = {
     novaTarefa: "",
-    tarefas: []
+    tarefas: [],
+    index: -1,
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
 
     novaTarefa = novaTarefa.trim();
@@ -25,9 +26,19 @@ export default class Main extends Component {
 
     const novasTarefas = [...tarefas];
 
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    })
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: "",
+      })
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      })
+    }
   }
 
   handleChange = (e) => {
@@ -37,7 +48,12 @@ export default class Main extends Component {
   }
 
   handleEdit = (e, index) => {
-    console.log("Edit", index);
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index]
+    })
   }
 
   handleDelete = (e, index) => {
